f71bf3ab9cb3ea334e4cd0cdbe9e76df01981b48
Dynamo Stack <9chakri+ds@gmail.com>
2016-08-04 03:13:31 -0400

    bin not need now

--- bin/commit.js
+++
@@ -1,7 +0,0 @@
-#! /usr/bin/env node
-var shell = require("shelljs");
-//console.log(process.platform,process.env.hasOwnProperty('ProgramFiles(x86)'));
-process.argv.shift();
-process.argv.shift();
-shell.exec("./bin/oc_mac "+process.argv.join(" "));
-
