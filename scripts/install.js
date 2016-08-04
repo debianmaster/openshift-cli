54cea7130f22dd5e2757c469aa30022256f648bb
Dynamo Stack <9chakri+ds@gmail.com>
2016-08-04 03:39:11 -0400

    v1

--- scripts/install.js
+++ scripts/install.js
@@ -8,4 +8,6 @@ if(process.platform!='darwin')
 url = "https://github.com/openshift/origin/releases/download/v1.2.1/openshift-origin-client-tools-v1.2.1-5e723f6-windows.zip"
 
 request(url).pipe(unzip.Extract({ path: 'assets/extracted' }));
+
+console.log("\n\nThis module only works for mac/windows only.. for linux use yum install openshift-cli")
 console.log("Detected "+process.platform+" Downloading client .. please wait...");
